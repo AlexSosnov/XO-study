c4b7f2b181183588c24fbb94dbdac6da6a6b3105
Aleksandr Sosnovskiy <alexsosnov@yandex.ru>
2020-10-01 13:09:11 +1000

    Field - created

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,4 +1,22 @@
 package io.hexlet.xo.model;
 
+import java.awt.*;
+
 public class Field {
+    private static final int FILE_SIZE = 3;
+
+    private  final  Figure[][] Field = new Figure[FILE_SIZE][FILE_SIZE];
+
+    public int getSize(){
+        return FILE_SIZE;
+    }
+
+    public Figure getFigure(final Point point){
+        return Field[point.x][point.y];
+    }
+
+    public void setFigure(final Point point, final Figure figure){
+        Field[point.x][point.y] = figure;
+
+    }
 }
