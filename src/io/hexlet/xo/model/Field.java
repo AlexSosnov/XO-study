61959a67a8e30b4dded9ecd239589486ee17d269
Aleksandr Sosnovskiy <alexsosnov@yandex.ru>
2020-10-03 13:26:36 +1000

    Field - created

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,22 +1,44 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyIqupidException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 public class Field {
-    private static final int FILE_SIZE = 3;
+    private static final int FIELD_SIZE = 3;
 
-    private  final  Figure[][] Field = new Figure[FILE_SIZE][FILE_SIZE];
+    private static final int MIN_COORDINATE = 0;
+
+    private static final int MAX_COORDINATE = FIELD_SIZE;
+
+    private  final  Figure[][] Field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize(){
-        return FILE_SIZE;
+        return FIELD_SIZE;
     }
 
-    public Figure getFigure(final Point point){
+    public Figure getFigure(final Point point) throws InvalidPointException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         return Field[point.x][point.y];
     }
 
-    public void setFigure(final Point point, final Figure figure){
+    public void setFigure(final Point point, final Figure figure) throws InvalidPointException,
+                                                                        AlreadyIqupidException {
+        if(!checkPoint(point)) {
+            throw new InvalidPointException();
+        }
         Field[point.x][point.y] = figure;
 
+    }
+
+    private boolean checkPoint(final Point point){
+        return checkCoordinat(point.x) && checkCoordinat(point.y);
+    }
+
+    private boolean checkCoordinat(final int coordinate){
+        return  coordinate >= MIN_COORDINATE && coordinate < FIELD_SIZE;
     }
 }
